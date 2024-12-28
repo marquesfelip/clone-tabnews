46208b6c8bb63eb47792e8f8dabe159f6719d5ca
Felipe Marques <felipehost@hotmail.com>
2024-12-28 18:42:42 -0300

    add `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,33 +1,44 @@
-import { Client } from "pg";
+import { Client } from 'pg'
 
 async function query(queryObject) {
+  let client
+  try {
+    client = await getNewClient()
+    const result = await client.query(queryObject)
+    return result
+  } catch (error) {
+    console.error(error)
+    throw error
+  } finally {
+    await client.end()
+  }
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    }
+  }
+  return process.env.NODE_ENV === 'production' ? true : false
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-  console.log('Postgres: ', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   })
 
-  try {
-    await client.connect();
-    const result = await client.query(queryObject);
-    return result;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
-    await client.end();
-  }
+  await client.connect()
+
+  return client
 }
 
 export default {
-  query: query,
-};
+  query,
+  getNewClient,
+}
